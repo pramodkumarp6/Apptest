a4ef758be1b4261e3374cb38c88f448108b36a1a
pramodkuarp6 <pramodkumarp6@gmail.com>
2020-09-25 20:43:34 +0530

    done

--- app/src/main/java/com/pramod/apptest/MainActivity.java
+++ app/src/main/java/com/pramod/apptest/MainActivity.java
@@ -1,5 +1,6 @@
 package com.pramod.apptest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        Thread t=new Thread() {
+            public void run() {
+
+                try {
+
+                    sleep(1000);
+
+
+                    Intent intent =new Intent(MainActivity.this,SignIn.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+
+
+                    finish();
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+
+        t.start();
     }
 }
+
+
